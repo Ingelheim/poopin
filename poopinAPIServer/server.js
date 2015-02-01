02f712832bd9e26c4f20b693bac0423beb81ae4c
Lukas von Ingelheim <lukas.ingelheim@gmail.com>
2015-02-01 19:05:36 +0100

    Try heroku

--- poopinAPIServer/server.js
+++ poopinAPIServer/server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app     = express();
 var server  = require('http').createServer(app);
 var io      = require('socket.io').listen(server);
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/index.html');
