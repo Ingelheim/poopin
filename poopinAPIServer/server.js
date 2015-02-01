8039a433961d7429254cb477b78a9a1d6c81fcfa
Lukas von Ingelheim <lukas.ingelheim@gmail.com>
2015-02-01 17:03:05 +0100

    Cleaned up server.js

--- poopinAPIServer/server.js
+++ poopinAPIServer/server.js
@@ -1,19 +1,3 @@
-//var app = require('express')();
-//var server = require('http').Server(app);
-//var io = require('socket.io')(server);
-//
-//server.listen(80);
-//
-//app.get('/', function (req, res) {
-//    res.sendfile(__dirname + '/index.html');
-//});
-//
-//io.on('connection', function (socket) {
-//    socket.emit('news', { hello: 'world' });
-//    socket.on('my other event', function (data) {
-//        console.log(data);
-//    });
-//});
 var express = require('express')
 var app     = express();
 var server  = require('http').createServer(app);
@@ -45,19 +29,5 @@ io.on('connection', function (socket) {
         io.emit('sendData3', { test: 'XXX' });
     });
 });
-
-//io.on('xxx', function (socket) {
-//    console.log("SSS");
-//    socket.emit('sendData3', { test: 'XXX' });
-//});
-
-
-//io.on('connection', function (socket) {
-//    console.log("connection");
-//    socket.emit('news', { hello: 'world' });
-//    //socket.on('my other event', function (data, error) {
-//    //    console.log(data);
-//    //});
-//});
 
 server.listen(8080);
